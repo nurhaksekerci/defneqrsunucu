8081f3fbf9f02e10b76ca5c3f3092d52b1f9f1c7
Nurhak <nurhaksekerci12@gmail.com>
2026-03-25 14:44:07 +0300

    feat: add SERVE_MEDIA environment variable to enable media serving in production and update URL patterns accordingly

--- backend-chp/config/urls.py
+++ backend-chp/config/urls.py
@@ -1,3 +1,5 @@
+import os
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -8,5 +10,11 @@ urlpatterns = [
     path('api/', include('chpist.urls')),
 ]
 
-if settings.DEBUG:
+_serve_media = os.environ.get("SERVE_MEDIA", "False").lower() in (
+    "1",
+    "true",
+    "yes",
+)
+
+if settings.DEBUG or _serve_media:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
