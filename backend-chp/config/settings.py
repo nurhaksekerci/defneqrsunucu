be596452f87b4137c7d2296e3e7a944344781ea3
Nurhak <nurhaksekerci12@gmail.com>
2026-03-25 18:43:18 +0300

    fix: update MEDIA_URL in settings.py to include leading slash for correct media file serving

--- backend-chp/config/settings.py
+++ backend-chp/config/settings.py
@@ -106,7 +106,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
